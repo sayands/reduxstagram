a1aec0c46480438c3d21f8cd1be0175cdae75d95
sayands <sayandsarkar.1997@gmail.com>
2018-04-05 18:46:11 +0530

    Added React Router Setup and Restructured Directories

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router";
+
+const PhotoGrid = React.createClass({
+	render() {
+		return <div className="photo-grid">I am the PhotoGrid</div>;
+	}
+});
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Single = React.createClass({
+	render() {
+		return <div className="single-photo">I am the single</div>;
+	}
+});
+
+export default Single;
