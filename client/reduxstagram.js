9f05545d90cf91d4bdb0619f0b1b1fb771cfbe40
sayands <sayandsarkar.1997@gmail.com>
2018-04-05 18:14:36 +0530

    Added Initial Setup

---
+++ client/reduxstagram.js
@@ -0,0 +1,8 @@
+// let's go!
+import React from "react";
+import { render } from "react-dom";
+
+//Import CSS
+import css from "./styles/style.styl";
+
+render(<p>Hello</p>, document.getElementById("root"));
