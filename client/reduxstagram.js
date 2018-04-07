6408b61464decce2302d7edd3dbbd22930fb8e12
sayands <sayandsarkar.1997@gmail.com>
2018-04-07 19:20:31 +0530

    Created Redux Store

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,4 +5,21 @@ import { render } from "react-dom";
 //Import CSS
 import css from "./styles/style.styl";
 
-render(<p>Hello</p>, document.getElementById("root"));
+//Import Components
+import Main from "./components/Main";
+import Single from "./components/Single";
+import PhotoGrid from "./components/PhotoGrid";
+
+//import react router deps
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+
+const router = (
+	<Router history={browserHistory}>
+		<Route path="/" component={Main}>
+			<IndexRoute component={PhotoGrid} />
+			<Route path="/view/:postId" component={Single} />
+		</Route>
+	</Router>
+);
+
+render(router, document.getElementById("root"));
