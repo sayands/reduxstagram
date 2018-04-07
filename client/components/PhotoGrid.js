8ba5389a8525d3686bf5db99bbb1c5fb6f09859e
sayands <sayandsarkar.1997@gmail.com>
2018-04-08 00:25:33 +0530

    Added Display Features

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { Link } from "react-router";
+import Photo from "./Photo";
 
 const PhotoGrid = React.createClass({
 	render() {
-		return <div className="photo-grid">I am the PhotoGrid</div>;
+		return (
+			<div className="photo-grid">
+				{this.props.posts.map((post, i) => (
+					<Photo {...this.props} key={i} i={i} post={post} />
+				))}
+			</div>
+		);
 	}
 });
 
