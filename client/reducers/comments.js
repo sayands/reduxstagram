9c3e77e967cf701a4a6af0bd64168d17c652f7c8
sayands <sayandsarkar.1997@gmail.com>
2018-05-17 12:33:59 +0300

    Added Redux Reducer Compostion

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,11 +1,33 @@
-// a reducer takes int two things :
-
-//1. action(info about what happened)
-
-//2. copy of current state
+function postComments(state = [], action) {
+  switch (action.type) {
+    case "ADD_COMMENT":
+      // return existing state with new comment
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment
+        }
+      ];
+    case "REMOVE_COMMENT":
+      console.log("Removing Comment");
+      //return without the deleted comment
+      return [...state.slice(0, action.i), ...state.slice(action.i + 1)];
+    default:
+      return state;
+  }
+  return state;
+}
 
 function comments(state = [], action) {
   //console.log(state, action);
+  if (typeof action.postId !== "undefined") {
+    return {
+      //take current state and overwrite post with new one
+      ...state,
+      [action.postId]: postComments(state[action.postId], action)
+    };
+  }
   return state;
 }
 
