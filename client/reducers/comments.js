bb2e60c5f94b8bb780fc6799bbbf73f2aaf4a1ca
sayands <sayandsarkar.1997@gmail.com>
2018-05-17 11:37:18 +0300

    Updated State with Reducers

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -5,8 +5,8 @@
 //2. copy of current state
 
 function comments(state = [], action) {
-	console.log(state, action);
-	return state;
+  //console.log(state, action);
+  return state;
 }
 
 export default comments;
