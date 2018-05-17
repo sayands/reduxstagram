728bd5ab783d7d3023934dec0ac18f2a1579d4ca
sayands <sayandsarkar.1997@gmail.com>
2018-05-17 11:57:14 +0300

    Added Display and Add Feature for Comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,8 +1,29 @@
 import React from "react";
 
 const Comments = React.createClass({
+  renderComment(comment, i) {
+    return (
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    );
+  },
+
   render() {
-    return <div className="comment">I am the comments !!!!</div>;
+    return (
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form className="comment-form" ref="commentForm">
+          <input type="text" ref="author" placeholder="author" />
+          <input type="text" ref="comment" placeholder="comment" />
+          <input type="submit" hidden />
+        </form>
+      </div>
+    );
   }
 });
 
--- client/components/Single.js
+++ client/components/Single.js
@@ -6,17 +6,16 @@ import Comments from "./Comments";
 const Single = React.createClass({
   render() {
     //index of the post
-    const i = this.props.posts.findIndex(
-      post => post.code === this.props.params.postId
-    );
+    const { postId } = this.props.params;
+    const i = this.props.posts.findIndex(post => post.code === postId);
     const post = this.props.posts[i];
-    console.log(post);
+    const postComments = this.props.comments[postId] || [];
+    //console.log(post);
 
-    //get us the post
     return (
       <div className="single-photo">
         <Photo i={i} post={post} {...this.props} />
-        <Comments />
+        <Comments postComments={postComments} />
       </div>
     );
   }
