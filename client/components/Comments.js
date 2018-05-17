ade0fe3e2f5d6e510140cc9fb76b76adf12afb0b
sayands <sayandsarkar.1997@gmail.com>
2018-05-17 11:47:10 +0300

    Added Display for a Single Photo Component

---
+++ client/components/Comments.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Comments = React.createClass({
+  render() {
+    return <div className="comment">I am the comments !!!!</div>;
+  }
+});
+
+export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { Link } from "react-router";
+import Photo from "./Photo";
+import Comments from "./Comments";
 
 const Single = React.createClass({
-	render() {
-		return <div className="single-photo">I am the single</div>;
-	}
+  render() {
+    //index of the post
+    const i = this.props.posts.findIndex(
+      post => post.code === this.props.params.postId
+    );
+    const post = this.props.posts[i];
+    console.log(post);
+
+    //get us the post
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments />
+      </div>
+    );
+  }
 });
 
 export default Single;
